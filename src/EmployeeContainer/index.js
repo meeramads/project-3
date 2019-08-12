cfd91b54f86b7d8ca46de1cf40a49fb8d945c306
Meera <meera.madhavan.95@gmail.com>
2019-08-11 19:33:18 -0700

    created container folder and index, including class, render, and export

---
+++ src/EmployeeContainer/index.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class EmployeeContainer extends Component {
+    constructor(){
+        super();
+        this.state = {
+            employees: []
+        }
+    }
+    
+
+    render(){
+        return (
+            <div>
+                Container
+            </div>
+        )
+    }
+}
+
+export default EmployeeContainer;
